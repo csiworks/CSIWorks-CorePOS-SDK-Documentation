a00e5dcf082b38f6a6f40225447b4fdf7956f545
csiworks-popailom <popailom@csiworks.net>
2025-08-25 13:21:31 +0300

    Deploy website - based on d34c9a54fa6efad3f8ba843e890fe624b34c01ac

---
+++ assets/js/a7456010.29a908f3.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkcore_pos_sdk=self.webpackChunkcore_pos_sdk||[]).push([[1235],{8552:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
