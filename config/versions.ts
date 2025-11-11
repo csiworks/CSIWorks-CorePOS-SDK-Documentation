e96c41f2ae051e44d88b05a3a00454e60cd8e19c
csiworks-popailom <popailom@csiworks.net>
2025-11-11 18:28:56 +0200

    Deploy website - based on 37c4fab3efbfeb56f81dbc58c8d79e20b59c907c

--- config/versions.ts
+++
@@ -1,8 +0,0 @@
-import type {VersionOptions} from '@docusaurus/plugin-content-docs';
-
-export const LAST_VERSION = '0.1.6';
-
-export const VERSIONS: Record<string, VersionOptions> = {
-  '0.1.6': { path: '0.1.6', label: '0.1.6', banner: 'none' },
-  '0.1.5': { path: '0.1.5', label: '0.1.5' }
-};
